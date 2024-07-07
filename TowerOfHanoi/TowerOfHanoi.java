4be4bfa03baa7c90b7940777546e9dd28efb56d9
sasindii <sasindiiwijethunga@gmail.com>
2024-07-07 20:11:55 +0530

    TowerOfHanoi
    Initial commit

---
+++ TowerOfHanoi/TowerOfHanoi.java
@@ -0,0 +1,16 @@
+public class TowerOfHanoi {
+    public static void hanoi(int n, char source, char auxiliary, char destination) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+        hanoi(n - 1, source, destination, auxiliary);
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+        hanoi(n - 1, auxiliary, source, destination);
+    }
+
+    public static void main(String[] args) {
+        int n = 3; // Number of disks
+        hanoi(n, 'S', 'A', 'D');
+    }
+}
